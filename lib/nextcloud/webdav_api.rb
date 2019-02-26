10ef7650b020586a57d1384c5dfc1b04d87343e1
Jaz Lim <angmeng@gmail.com>
2019-02-26 22:30:52 +0800

    Update path to prepend nextcloud

--- lib/nextcloud/webdav_api.rb
+++ lib/nextcloud/webdav_api.rb
@@ -8,7 +8,7 @@ module Nextcloud
     # @params args [Hash] Hash with url, username and password
     def initialize(args)
       super
-      @url = URI(@url.scheme + "://" + @url.host + "/" + DAV_URL)
+      @url = URI(@url.scheme + "://" + @url.host + "/nextcloud/" + DAV_URL)
     end
 
     # Initiates WebDAV Directory class
